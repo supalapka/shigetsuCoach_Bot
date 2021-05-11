94274756c68f6a83b54ba41e297d0686f15aac66
supalapka <yurabalint0103@gmail.com>
2021-05-11 22:02:46 +0300

    some info

--- shigetsuCoach_Bot/Models/Commands/CoachingCommand.cs
+++ shigetsuCoach_Bot/Models/Commands/CoachingCommand.cs
@@ -1,5 +1,8 @@
 ﻿using shigetsuCoach_Bot.Commands;
 using shigetsuCoach_Bot.Models.Contollers;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -9,14 +12,33 @@ namespace shigetsuCoach_Bot.Models.Commands
     {
         public override string Name => "coaching";
 
-        public override void ExecuteAsync(Message msg, TelegramBotClient client)
+        public override async void ExecuteAsync(Message msg, TelegramBotClient client)
         {
             var chatId = msg.Chat.Id;
             var messageId = msg.MessageId;
 
 
-            CoachingController coachingContoller = new CoachingController(msg, client);
+            string output = "Почему тебе стоит заказать разбор реплея?\n" +
+                "\n" +
+                "В интернете почти нет людей которые с рангом 140 будут разбирать твой реплей в дискорде.\n" +
+                "Аналоги разборов за такую цену можно найти от 6-7к игроков(1500 ранг) которым явно далеко до игры с про игроками.\n" +
+                "\n" +
+                "Имея 140 ранг я уже играл в стаках с чего имею опыт профессиональной доты и даже обыгрывал винстрайк на квалах (2020 год, 600 ранг)";
 
+
+            var imagePath1 = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Data\Files\winstrike1.png");
+            var imagePath2 = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Data\Files\winstrike2.png");
+            await client.SendTextMessageAsync(chatId, output);
+
+            InputMediaPhoto inputMediaPhoto1 = new InputMediaPhoto(imagePath1);
+            InputMediaPhoto inputMediaPhoto2 = new InputMediaPhoto(imagePath2);
+
+            InputMediaBase media = new InputMediaBase(imagePath1);
+
+
+            await client.SendMediaGroupAsync(chatId, (inputMediaPhoto1, inputMediaPhoto2));
+
+            CoachingController coachingContoller = new CoachingController(msg, client);
             coachingContoller.MainMenu();
         }
     }
