279e228f89f209d431fcc90dd1048995b77225b1
supalapka <yurabalint0103@gmail.com>
2021-05-11 11:45:51 +0300

    migrations

---
+++ shigetsuCoach_Bot/Migrations/202105110843356_firstMigration.cs
@@ -0,0 +1,37 @@
+﻿namespace shigetsuCoach_Bot.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Reviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ReviewString = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserTelegramId = c.Int(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Reviews");
+        }
+    }
+}
---
+++ shigetsuCoach_Bot/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace shigetsuCoach_Bot.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<shigetsuCoach_Bot.Data.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(shigetsuCoach_Bot.Data.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
