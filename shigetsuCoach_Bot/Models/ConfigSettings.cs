8be824573e393dac97c5764ba91bb682f4645d90
supalapka <yurabalint0103@gmail.com>
2021-05-11 10:13:29 +0300

    clean config

--- shigetsuCoach_Bot/Models/ConfigSettings.cs
+++ shigetsuCoach_Bot/Models/ConfigSettings.cs
@@ -3,10 +3,10 @@ namespace shigetsuCoach_Bot.Models
 {
     public static class ConfigSettings
     {
-        public static string Token { get; set; } = "1737896640:AAFoeI0IO9sTuzp0d9JAkv_klNZJFUqEiIs";
-        public static long supalapkaId { get; set; } = 747969117;
-        public static string supalapkaName { get; set; } = "manager";
-        public static string supalapkaPhone { get; set; } = "+380509716095";
-        public static long shigetsuId { get; set; } = 443521128;
+        public static string Token { get; set; } = "{BOT TOKEN}";
+        public static long supalapkaId { get; set; } = -1;
+        public static string supalapkaName { get; set; } = "-1";
+        public static string supalapkaPhone { get; set; } = "{phone number}";
+        public static long shigetsuId { get; set; } = -1;
     }
 }
