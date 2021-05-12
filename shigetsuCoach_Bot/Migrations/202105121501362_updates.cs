bcde2be548078a96d42027659b73a95289fa4935
supalapka <yurabalint0103@gmail.com>
2021-05-12 18:02:52 +0300

    EF migration

---
+++ shigetsuCoach_Bot/Migrations/202105121501362_updates.cs
@@ -0,0 +1,20 @@
+﻿namespace shigetsuCoach_Bot.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updates : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "firstName", c => c.String());
+            DropColumn("dbo.Users", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Users", "Name", c => c.String());
+            DropColumn("dbo.Users", "firstName");
+        }
+    }
+}
