7c06b165dfd33cd8bc1c9d3c27c80776103d433d
supalapka <yurabalint0103@gmail.com>
2021-05-14 18:02:04 +0300

    quick payment fix

--- shigetsuCoach_Bot/Program.cs
+++ shigetsuCoach_Bot/Program.cs
@@ -1,17 +1,14 @@
 ﻿using shigetsuCoach_Bot.Commands;
 using shigetsuCoach_Bot.Contollers;
-using shigetsuCoach_Bot.Data;
-using shigetsuCoach_Bot.Models; 
+using shigetsuCoach_Bot.Models;
 using shigetsuCoach_Bot.Models.Commands;
 using shigetsuCoach_Bot.Models.Commands.PrivateCommands;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace shigetsuCoach_Bot
@@ -19,7 +16,7 @@ namespace shigetsuCoach_Bot
     class Program
     {
         public static bool isMainMenu = true;
-        public static  bool botWorks = true;
+        public static bool botWorks = true;
 
 
         private static List<Command> commandsList;
@@ -27,8 +24,8 @@ namespace shigetsuCoach_Bot
 
         private static TelegramBotClient client;
 
-           // public static async Task Main()
-            public static void Main()
+        // public static async Task Main()
+        public static void Main()
         {
             client = new TelegramBotClient(ConfigSettings.Token);
 
@@ -52,11 +49,11 @@ namespace shigetsuCoach_Bot
             client.OnCallbackQuery += BotOnCallBackQueryAsync;
 
             // Console.ReadLine();
-            while (botWorks)  { }
+            while (botWorks) { }
             client.StopReceiving();
         }
 
-        private static  void BotOnCallBackQueryAsync(object sender, CallbackQueryEventArgs e)
+        private static void BotOnCallBackQueryAsync(object sender, CallbackQueryEventArgs e)
         {
             var callback = e.CallbackQuery.Data;
             var msg = e.CallbackQuery.Message;
@@ -65,22 +62,24 @@ namespace shigetsuCoach_Bot
 
             foreach (var command in commands)
             {
-                if (command.Name ==(callback))
+                if (command.Name == (callback))
                 {
                     command.ExecuteAsync(msg, client);
                     break;
                 }
             }
 
-            foreach (var command in commands) //payment confirm
-            {
-                if (command.Contains("Confirm"))
+
+            if (callback.Contains("Confirm"))//payment confirm
+                foreach (var command in commands)
                 {
-                    msg.Text = callback;
-                    command.ExecuteAsync(msg, client);
-                    break;
+                    if (callback.Contains(command.Name))
+                    {
+                        msg.Text = callback;
+                        command.ExecuteAsync(msg, client);
+                        break;
+                    }
                 }
-            }
         }
 
         private static async void BotOnMessageReceived(object sender, MessageEventArgs e)
@@ -89,21 +88,21 @@ namespace shigetsuCoach_Bot
 
             if (msg.Type == Telegram.Bot.Types.Enums.MessageType.Contact) // contacts logic
             {
-                await client.SendContactAsync(ConfigSettings.supalapkaId, msg.Contact.PhoneNumber,msg.Contact.FirstName,msg.Contact.LastName);
-                await client.SendTextMessageAsync(msg.Chat.Id, "Контакт добавлен в список", replyMarkup:new ReplyKeyboardRemove());
+                await client.SendContactAsync(ConfigSettings.shigetsuId, msg.Contact.PhoneNumber, msg.Contact.FirstName, msg.Contact.LastName);
+                await client.SendTextMessageAsync(msg.Chat.Id, "Контакт добавлен в список", replyMarkup: new ReplyKeyboardRemove());
                 MakeOrderController makeOrderController = new MakeOrderController(msg, client);
                 makeOrderController.SetPayment();
 
-                ReviewsController reviewsController = new ReviewsController(msg, client); 
+                ReviewsController reviewsController = new ReviewsController(msg, client);
                 reviewsController.SaveUser(); //save user func in reviews....
             }
 
-            else if(msg.Type == Telegram.Bot.Types.Enums.MessageType.Photo) //photo logic. sends photo / paymentscreen to shigetsu
+            else if (msg.Type == Telegram.Bot.Types.Enums.MessageType.Photo) //photo logic. sends photo / paymentscreen to shigetsu
             {
                 var screen = await client.GetFileAsync(msg.Photo[msg.Photo.Count() - 1].FileId);
                 ConfirmPaymentContoller confirmPaymentContoller = new ConfirmPaymentContoller(msg, client);
                 confirmPaymentContoller.SendShigetsuScreen(screen, msg.Chat.Id);
-                await client.SendTextMessageAsync(msg.Chat.Id,"Скриншот отправлен на проверку, сразу же после успешной проверки вам напишут про реплей");
+                await client.SendTextMessageAsync(msg.Chat.Id, "Скриншот отправлен на проверку, сразу же после успешной проверки вам напишут про реплей");
             }
 
             if (msg.Text != null)
